afaea0f7f9ad53e3119052530d53f982957e220b
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-01-02 13:42:12 -0500

    msg

--- imem/model/trial.py
+++ imem/model/trial.py
@@ -16,7 +16,7 @@ class IMemTrial(pytry.NengoTrial):
         self.param("item dimensionality", item_d=256)
         self.param("context dimensionality", context_d=256)
         self.param("contextual drift rate", beta=0.62676)
-        self.param("distractor rate", distractor_rate=.5)
+        self.param("distractor rate", distractor_rate=.4)
         self.param("OSE memory decay", gamma=0.9775)
         self.param("OSE memory threshold", ose_thr=0.1)
         self.param("TCM prob. to recall from beginning", ordinal_prob=.1)
@@ -39,48 +39,48 @@ class IMemTrial(pytry.NengoTrial):
             self.p_recalls = nengo.Probe(model.imem.output, synapse=0.01)
             self.p_pos = nengo.Probe(model.imem.output_pos, synapse=0.01)
 
-            self.debug_probes = {
-                'recall_state': model.imem.recall.state.output,
-                'recall_threshold': model.imem.recall.threshold.heaviside,
-                'recall_buf': model.imem.recall.buf.output,
-                'pos_recall_state': model.imem.pos_recall.state.output,
-                'pos_recall_buf': model.imem.pos_recall.buf.output,
-                'aml_comp': model.imem.tcm.net_m_tf.compare.output,
-                'ctx': model.imem.tcm.current_ctx.output,
-                'ctx_update': model.imem.tcm.current_ctx.input_update_context,
-                'inhib_recall': model.imem.recall.inhibit.output,
-                'out_inhibit_gate': model.imem.recall.out_inhibit_gate.output,
-                'recall_ctx': model.imem.tcm.net_m_ft.output,
-                'recall_ctx_cue': model.imem.tcm.net_m_ft.input_cue,
-                'input_pos': model.imem.tcm.input_pos,
-                'current_ctx': model.imem.tcm.current_ctx.output,
-                'input_update_ctx': model.imem.tcm.input_update_context,
-                'sim_th': model.imem.sim_th.output,
-                'last_item': model.imem.last_item.output,
-                'ose_output': model.imem.ose.output,
-                'tcm_output': model.imem.tcm.net_m_tf.output,
-                'failed_recall_int': model.imem.recall.failed_recall_int,
-                'failed_recall': model.imem.recall.failed_recall,
-                'failed_recall_heaviside':
-                    model.imem.recall.failed_recall_heaviside,
-                'start_of_serial_recall': model.imem.start_of_serial_recall,
-                'start_of_free_recall': model.imem.start_of_free_recall,
-                'pos_state': model.imem.pos.state.output,
-                'pos_state_in': model.imem.pos.state.input,
-                'pos_inhibit_threshold': model.imem.pos.inhibit_threshold.output,
-                'pos_advance_threshold': model.imem.pos.advance_threshold.output,
-                'input_inc': model.imem.pos.input_inc,
-                'no_pos_count': model.imem.ctrl.output_no_pos_count,
-                'ose_recall_gate': model.imem.ose_recall_gate.output,
-                'tcm_recall_gate': model.imem.tcm_recall_gate.output,
-                'pos_gate': model.imem.pos_gate.output,
-                'ose': model.imem.ose.mem.output,
-                'buf_input_store': model.imem.recall.buf_input_store,
-                # 'out_inhib_gate_update': model.imem.recall.out_inhib_gate_update,
-                # 'input_update_inhibit': model.imem.recall.input_update_inhibit,
-                'sim_th_neg': model.imem.sim_th_neg,
-                'sim_th_pos': model.imem.sim_th_pos,
-            }
+            # self.debug_probes = {
+                # 'recall_state': model.imem.recall.state.output,
+                # 'recall_threshold': model.imem.recall.threshold.heaviside,
+                # 'recall_buf': model.imem.recall.buf.output,
+                # 'pos_recall_state': model.imem.pos_recall.state.output,
+                # 'pos_recall_buf': model.imem.pos_recall.buf.output,
+                # 'aml_comp': model.imem.tcm.net_m_tf.compare.output,
+                # 'ctx': model.imem.tcm.current_ctx.output,
+                # 'ctx_update': model.imem.tcm.current_ctx.input_update_context,
+                # 'inhib_recall': model.imem.recall.inhibit.output,
+                # 'out_inhibit_gate': model.imem.recall.out_inhibit_gate.output,
+                # 'recall_ctx': model.imem.tcm.net_m_ft.output,
+                # 'recall_ctx_cue': model.imem.tcm.net_m_ft.input_cue,
+                # 'input_pos': model.imem.tcm.input_pos,
+                # 'current_ctx': model.imem.tcm.current_ctx.output,
+                # 'input_update_ctx': model.imem.tcm.input_update_context,
+                # 'sim_th': model.imem.sim_th.output,
+                # 'last_item': model.imem.last_item.output,
+                # 'ose_output': model.imem.ose.output,
+                # 'tcm_output': model.imem.tcm.net_m_tf.output,
+                # 'failed_recall_int': model.imem.recall.failed_recall_int,
+                # 'failed_recall': model.imem.recall.failed_recall,
+                # 'failed_recall_heaviside':
+                    # model.imem.recall.failed_recall_heaviside,
+                # 'start_of_serial_recall': model.imem.start_of_serial_recall,
+                # 'start_of_free_recall': model.imem.start_of_free_recall,
+                # 'pos_state': model.imem.pos.state.output,
+                # 'pos_state_in': model.imem.pos.state.input,
+                # 'pos_inhibit_threshold': model.imem.pos.inhibit_threshold.output,
+                # 'pos_advance_threshold': model.imem.pos.advance_threshold.output,
+                # 'input_inc': model.imem.pos.input_inc,
+                # 'no_pos_count': model.imem.ctrl.output_no_pos_count,
+                # 'ose_recall_gate': model.imem.ose_recall_gate.output,
+                # 'tcm_recall_gate': model.imem.tcm_recall_gate.output,
+                # 'pos_gate': model.imem.pos_gate.output,
+                # 'ose': model.imem.ose.mem.output,
+                # 'buf_input_store': model.imem.recall.buf_input_store,
+                # # 'out_inhib_gate_update': model.imem.recall.out_inhib_gate_update,
+                # # 'input_update_inhibit': model.imem.recall.input_update_inhibit,
+                # 'sim_th_neg': model.imem.sim_th_neg,
+                # 'sim_th_pos': model.imem.sim_th_pos,
+            # }
             if p.debug:
                 for k in self.debug_probes:
                     self.debug_probes[k] = nengo.Probe(
@@ -122,15 +122,16 @@ class IMemTrial(pytry.NengoTrial):
 
         result = {
             'responses': responses,
-            'pos': sim.data[self.p_pos],
-            'recalls': sim.data[self.p_recalls],
-            'positions': positions,
-            'vocab_vectors': self.vocabs.items.vectors,
-            'vocab_keys': list(self.vocabs.items.keys()),
-            'pos_vectors': self.vocabs.positions.vectors,
-            'pos_keys': list(self.vocabs.positions.keys()),
+            # 'pos': sim.data[self.p_pos],
+            # 'recalls': sim.data[self.p_recalls],
+            # 'positions': positions,
+            # 'vocab_vectors': self.vocabs.items.vectors,
+            # 'vocab_keys': list(self.vocabs.items.keys()),
+            # 'pos_vectors': self.vocabs.positions.vectors,
+            # 'pos_keys': list(self.vocabs.positions.keys()),
         }
         if p.debug:
             result.update(
                 {k: sim.data[v] for k, v in self.debug_probes.items()})
+        print(result)
         return result
