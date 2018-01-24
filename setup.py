b1e784ab2b34f638e00cc131c847a4d84288e86b
Jan Gosmann <jgosmann@uwaterloo.ca>
2018-01-24 16:48:10 -0500

    Use https URL for nengo_extras dependency.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'statsmodels',
     ],
     dependency_links=[
-        'git+ssh://git@github.com/nengo/nengo_extras.git'
+        'git+https://git@github.com/nengo/nengo_extras.git'
         '@d63e12aa787419fcafed32027105583d614e9e6d#egg=nengo-extras-0.1.0.dev0'
     ]
 )
